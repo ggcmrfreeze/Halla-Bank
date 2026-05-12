c1dc3f6d54a51942ce6d7c5ba60844a8181368a1
Михаил <misaaputunan@gmail.com>
2026-05-12 11:35:11 +0300

    Закончена верста сайта

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.insurance-slider', {
-        // Количество слайдов для показа
         slidesPerView: 1,
         spaceBetween: 20,
 
-        // Навигация
         navigation: {
             nextEl: '.swiper-button-next-custom',
             prevEl: '.swiper-button-prev-custom',
         },
 
-        // Адаптивность (настраиваем сколько карточек видно)
         breakpoints: {
-            // от 768px - 2 карточки
             768: {
                 slidesPerView: 2,
             },
-            // от 1024px - 3 карточки
             1024: {
                 slidesPerView: 3,
             }
